e1a0938c6249d5c1ef616128ec649a3838cabfcf
Unknown <l.b.vandenhaak@gmail.com>
2018-03-12 11:38:34 +0100

    Cycle tracking and a bit of code cleaning

--- hackcel/src/Data/Hackcel/Core/Eval.hs
+++ hackcel/src/Data/Hackcel/Core/Eval.hs
@@ -28,20 +28,24 @@ newtype Eval field value error a = Eval {
   runEvalState :: ExceptT error (State (EvalState field value error)) a
 } deriving (Monad, Functor, Applicative)
 
-runEval :: Eval field value e a -> EvalState field value e -> (Either e a, EvalState field value e)
+runEval :: Eval field value e a -> EvalState field value e
+        -> (Either e a, EvalState field value e)
 runEval = runState . runExceptT . runEvalState
 
-runField :: (HackcelError error field, Ord field) => field -> HackcelState field value error -> (Either error value, HackcelState field value error)
+runField :: (HackcelError error field, Ord field) => field
+        -> HackcelState field value error
+        -> (Either error value, HackcelState field value error)
 runField f hackcel = (result, finalHackcel)
   where
     initial = EvalState hackcel f []
-    (result, (EvalState finalHackcel _ _)) = runEval (evalExpression f) initial
+    (result, EvalState finalHackcel _ _) = runEval (evalExpression f) initial
 
 data Argument field value error
   = AValue (Eval field value error value)
   | ARange field field
 
-getValue :: (HackcelError error field, Ord field) => field -> Eval field value error value
+getValue :: (HackcelError error field, Ord field) => field
+         -> Eval field value error value
 getValue f = Eval $
   do
     s <- get
@@ -53,44 +57,88 @@ getValue f = Eval $
         Left e -> throwError e
         Right x -> return x
 
-evalExpression :: (HackcelError error field, Ord field) => field -> Eval field value error value
-evalExpression f = Eval $
+evalExpression :: (HackcelError error field, Ord field) => field
+               -> Eval field value error value
+evalExpression fld = Eval $
   do
     s <- get
     let EvalState { esHackcelState = HackcelState m funcs } =  s
-    case M.lookup f m of
-      Nothing -> throwError (errorUnknownField f)
+    case M.lookup fld m of
+      Nothing -> throwError (errorUnknownField fld)
       Just (expr, _) ->
         do
+          -- Add field to the stack, throws error when already in stack
+          runEvalState $ updateStack fld
+          -- Get the result (might go into another evalExpression call)
           tres <- runEvalState (evalExpr' expr)
-          let res = Just (FieldResult (Right tres) (referencedFields expr))
-          let newm = M.insert f (expr,res) m
-          put $ s {esHackcelState = HackcelState newm funcs}
+          -- Remove field from the stack again
+          runEvalState popStack
+          -- Update the state with the result
+          runEvalState $ updateResult fld tres
           return tres
         `catchError` (\e ->
           do
             let res = Just $ FieldResult (Left e) []
-            let newm = M.insert f (expr,res) m
+            let newm = M.insert fld (expr,res) m
             put $ s {esHackcelState = HackcelState newm funcs}
             throwError e)
   where
-    evalExpr' :: (HackcelError error field, Ord field) => Expression field value error -> Eval field value error value
+    evalExpr' :: (HackcelError error field, Ord field)
+              => Expression field value error -> Eval field value error value
     evalExpr' (ExprLit val)  = return val
     evalExpr' (ExprField f2) = getValue f2
-    evalExpr' (ExprApp f args) = Eval $ do
+    evalExpr' (ExprApp fld args) = Eval $ do
       s <- get
       let EvalState { esHackcelState = HackcelState m funcs } =  s
-      runEvalState $ funcs f (Prelude.map toArgument args)
-    toArgument ::  (HackcelError error field, Ord field) => Parameter field value error -> Argument field value error
-    toArgument (PRange f1 f2) = ARange f1 f2
+      runEvalState $ funcs fld (Prelude.map toArgument args)
+
+    toArgument :: (HackcelError error field, Ord field)
+               => Parameter field value error -> Argument field value error
+    toArgument (PRange fld1 fld2) = ARange fld1 fld2
     toArgument (PExpr e)      = AValue $ evalExpr' e
+
+    updateStack :: (HackcelError error field, Ord field) => field
+                -> Eval field value error ()
+    updateStack fld2 = Eval $
+      do
+        s <- get
+        let EvalState { esStack = stack } =  s
+        if fld2 `elem` stack
+          then throwError (errorRecursion stack)
+          else put $ s {esStack = fld2 : stack}
+
+    popStack :: (HackcelError error field, Ord field)
+             => Eval field value error ()
+    popStack = Eval $
+      do
+        s <- get
+        let EvalState { esStack = stack } =  s
+        case stack of
+          []       -> error "This should really not happen, the stack is empty when trying to pop"
+          (_:news) -> put $ s {esStack = news}
+
+updateResult :: (HackcelError error field, Ord field) => field
+             -> value -> Eval field value error ()
+updateResult fld val = Eval $
+  do
+    s <- get
+    let EvalState { esHackcelState = HackcelState m funcs } =  s
+    case M.lookup fld m of
+      Nothing -> throwError (errorUnknownField fld)
+      Just (expr, _) ->
+        do
+          let res = Just (FieldResult (Right val) (referencedFields expr))
+          let newm = M.insert fld (expr, res) m
+          put $ s {esHackcelState = HackcelState newm funcs}
+
 
 referencedFields :: Expression field value error -> [field]
 referencedFields (ExprField f) = [f]
 referencedFields (ExprLit _) = []
 referencedFields (ExprApp _ _) = []
 
-dependencies :: (HackcelError error field, Ord field) => field -> Eval field value error [field]
+dependencies :: (HackcelError error field, Ord field) => field
+             -> Eval field value error [field]
 dependencies field = Eval $ do
           s <- get
           let EvalState { esHackcelState = HackcelState m funcs } =  s
@@ -102,4 +150,3 @@ dependencies field = Eval $ do
                         let FieldResult { fieldDependants = depends } = fr
                         return depends
   -- TODO: Do ExprLetIn
-
